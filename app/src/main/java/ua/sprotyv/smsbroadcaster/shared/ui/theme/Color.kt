1383f39cf6aec345ef1eb66590161d2559db2ad7
Oleksii Malovanyi <oleksii.malovanyi@gmail.com>
2022-03-12 19:42:44 +0200

    Change default colors

--- app/src/main/java/ua/sprotyv/smsbroadcaster/shared/ui/theme/Color.kt
+++ app/src/main/java/ua/sprotyv/smsbroadcaster/shared/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package ua.sprotyv.smsbroadcaster.shared.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Yellow200 = Color(0xfffff176)
+val Yellow500 = Color(0xFFffffa8)
+val Yellow700 = Color(0xffcabf45)
+val Greaen200 = Color(0xff9ccc65)
